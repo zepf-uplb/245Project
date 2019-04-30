9356916a9cd3738a776223903f96e56044c1a920
Franz Joezepf C. Dinglasan <zepf.uplb@gmail.com>
2019-04-30 19:12:51 +0800

    intersection, randomize, brightness
    Plus modularized code.

--- main.py
+++ main.py
@@ -1,27 +1,82 @@
+import random
 
+random.seed(11)
 
 def main():
 	lines = [line.rstrip('\n') for line in open("Hashi_Puzzles/100/Hs_16_100_25_00_001.has")]
+	size = int(lines[0].split()[0])
 	islands = []
 	variables = []
-	size = int(lines[0].split()[0])
+	intersections = []	
 
-	for i in range(1, len(lines)):
-		for j, k in enumerate(lines[i].split()):
-			if k != "0":
-				islands.insert(len(islands), ((i-1, j), int(k)))
+	createIslands(lines, islands)
+	generateVariables(size, islands, variables)	
+	findIntersections(islands, variables, intersections)
+	randomizeSolution(variables)	
+	brightness = determineBrightness(size, islands, variables)
+
+	'''
+	print("++++++++++++++++++++")
+	print(intersections)
+	print("++++++++++++++++++++")
+	print(islands[variables[8][0]], islands[variables[8][1]])
+	print(islands[variables[102][0]], islands[variables[102][1]])
+	'''	
+		
+	print(brightness)
 	
+
+def createIslands(lines, islands):
+	for i in range(1, len(lines)):
+		for x, y in enumerate(lines[i].split()):
+			if y != "0":
+				islands.insert(len(islands), ((i-1, x), int(y)))
+
+def generateVariables(size, islands, variables):
 	for i in range(size):
 		row = [(x,y) for (x,y) in enumerate(islands) if y[0][0] == i]
 		for j in range(len(row)-1):
-			variables.insert(len(variables), (row[j][0], row[j+1][0], 0))
+			variables.insert(len(variables), [row[j][0], row[j+1][0], 0])
 
 	for i in range(size):
 		col = [(x,y) for (x,y) in enumerate(islands) if y[0][1] == i]
 		for j in range(len(col)-1):
-			variables.insert(len(variables), (col[j][0], col[j+1][0], 0))
+			variables.insert(len(variables), [col[j][0], col[j+1][0], 0])
 
-	print(variables)
+def findIntersections(islands, variables, intersections):
+	for i in range(len(variables)-1):
+		for j in range(i+1, len(variables)):
+			if len(set(variables[i][:2]) & set(variables[j][:2])) > 0:
+				continue				
+			elif intersect(islands[variables[i][0]], islands[variables[i][1]], \
+				islands[variables[j][0]], islands[variables[j][1]]):
+
+				intersections.insert(len(intersections), (i, j))
+
+def randomizeSolution(variables):
+	for x in variables:
+		x[2] = random.randint(0, 2)
+
+def determineBrightness(size, islands, variables):
+	brightness = 0	
+	for i in range(size):
+		sum = 0
+		interest = [x for x in variables if x[0] == i or x[1] == i]	
+		for x in interest:
+			sum += x[2]
+		brightness += abs(islands[i][1] - sum)
+
+	return brightness
+
+def ccw(A, B, C):
+	return (C[0][1]-A[0][1]) * (B[0][0]-A[0][0]) > (B[0][1]-A[0][1]) * (C[0][0]-A[0][0])
+
+def intersect(A, B, C, D):
+	return ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D)
+
+def nand(A, B):
+	return not(A and B)
+
 
 if __name__ == "__main__":
 	main()
